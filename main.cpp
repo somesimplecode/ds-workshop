8cc0ccf9a6f42ad73c1421e2ced1d78cc3e73176
somesimplecode <esochaghib2024@gmail.com>
2024-10-04 14:30:21 -0400

    Adding basic functionality

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <cstdlib>
+
+int main(int argc, char* argv[])
+{
+    std::list<int> i_list;
+
+    for(for i = 1; i < argc; ++i)
+    {
+        int temp = std::atoi(argv[i]);
+        i_list.push_back(temp);
+    }
+
+    std::list<int>::iterator it = i_List.begin();
+    while (it != i_List.end())
+    {
+        std::cout << *it << std::endl;
+        it++;
+    }
+
+    return 0;
+}
+
+#endif
